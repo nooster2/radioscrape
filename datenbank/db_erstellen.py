a002bdf4819daf324ddba30865a8720f79b042d7
alex <alexander.born@outlook.com>
2018-03-10 15:52:42 +0100

    Änderung der Datenbank-Struktur: Aufteilung in die Tabellen bayern3 und songs (s. create-statements in db_erstellen.py)

--- datenbank/db_erstellen.py
+++ datenbank/db_erstellen.py
@@ -1,26 +1,39 @@
 import sqlite3
 
-connection = sqlite3.connect('db.sqlite3')
+connection = sqlite3.connect('../db.sqlite3')
 cursor = connection.cursor()
 
 cursor.execute("""
     CREATE TABLE bayern3 (
         id INTEGER,
         datum_zeit TEXT,
-        interpret TEXT,
-        titel TEXT,
+        song INTEGER,
         PRIMARY KEY (id),
-        UNIQUE (datum_zeit, interpret, titel)
+        FOREIGN KEY(song) REFERENCES songs(id),
+        UNIQUE (datum_zeit, song)
     );
 """)
 
 cursor.execute("""
+    CREATE TABLE songs (
+        id INTEGER,
+        interpret TEXT,
+        titel TEXT,
+        veroeffentlichung TEXT,
+        label TEXT,
+        PRIMARY KEY (id),
+        UNIQUE (interpret, titel)
+        );
+    """)
+
+cursor.execute("""
     CREATE TABLE letztesParsen (
         id INTEGER,
+        sender TEXT,
         url TEXT,
         PRIMARY KEY (id)
-    );
-""")
+        );
+    """)
 
 connection.commit()
 connection.close()
--- datenbank/db_umziehen.py
+++ datenbank/db_umziehen.py
@@ -1,24 +1,48 @@
 import sqlite3
 
-conn_alt = sqlite3.connect('datenbank.db')
+# Connection zur alten Datenbank
+conn_alt = sqlite3.connect('../db_alt.sqlite3')
+conn_alt.row_factory = sqlite3.Row
 cursor_alt = conn_alt.cursor()
 
-conn_neu = sqlite3.connect('db.sqlite3')
+# Connection zur neuen Datenbank
+conn_neu = sqlite3.connect('../db.sqlite3')
+conn_neu.row_factory = sqlite3.Row
 cursor_neu = conn_neu.cursor()
 
+# Daten aus alter Datenbank laden und in neue schreiben
 bayern3 = cursor_alt.execute('SELECT * FROM bayern3').fetchall()
 for eintrag in bayern3:
-    cursor_neu.execute("""
-        INSERT INTO bayern3 (datum_zeit, interpret, titel)
-        VALUES (?,?,?);
-    """, eintrag)
+    try:
+        cursor_neu.execute("""
+            INSERT INTO songs (interpret, titel)
+            VALUES (?,?);
+            """, (eintrag['interpret'], eintrag['titel']))
+    
+    except sqlite3.IntegrityError: # Wenn der Titel schon in songs enthalten ist
+        pass
 
-letztesParsen = cursor_alt.execute('SELECT url FROM letzte_url').fetchall()
+    try:
+        song = cursor_neu.execute("""SELECT id FROM songs WHERE interpret = ? AND titel = ?;""", (eintrag['interpret'], eintrag['titel'])).fetchone()
+        cursor_neu.execute("""
+            INSERT INTO bayern3 (datum_zeit, song)
+            VALUES (?,?);
+            """, (eintrag['datum_zeit'], song[0]))
+    
+    except TypeError: # Nötig, da teilweise der Interpret leer ist und stattdessen beim Titel mit dabei steht
+        print(eintrag['datum_zeit'], ' ', eintrag['interpret'], ' ', eintrag['titel'])
+        song = cursor_neu.execute("""SELECT id FROM songs WHERE titel = ?;""", (eintrag['titel'],)).fetchone()
+        cursor_neu.execute("""
+            INSERT INTO bayern3 (datum_zeit, song)
+            VALUES (?,?);
+            """, (eintrag['datum_zeit'], song[0]))
+    
+letztesParsen = cursor_alt.execute('SELECT url FROM letztesParsen').fetchall()
 for eintrag in letztesParsen:
     cursor_neu.execute("""
         INSERT INTO letztesParsen (url)
         VALUES (?);
-    """, eintrag)
+        """, eintrag)
 
 
 conn_neu.commit()
